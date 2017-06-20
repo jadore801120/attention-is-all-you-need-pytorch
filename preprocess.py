9c32388bdf75ed428526e4fa150ff83a7da3fbf8
Victor Huang <jadore801120@gmail.com>
2017-06-20 15:03:29 +0800

    remove global variable

--- preprocess.py
+++ preprocess.py
@@ -3,35 +3,22 @@ import argparse
 import torch
 import transformer.Constants as Constants
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-train_src', required=True)
-parser.add_argument('-train_tgt', required=True)
-parser.add_argument('-valid_src', required=True)
-parser.add_argument('-valid_tgt', required=True)
-parser.add_argument('-output', required=True)
-parser.add_argument('-max_seq_len', type=int, default=20)
-parser.add_argument('-min_word_count', type=int, default=5)
-parser.add_argument('-keep_case', action='store_true')
-parser.add_argument('-share_vocab', action='store_true')
-
-opt = parser.parse_args()
-
-def read_instances_from_file(inst_file):
+def read_instances_from_file(inst_file, max_seq_len, keep_case):
     ''' Convert file into word seq lists and vocab '''
 
     word_insts = []
     with open(inst_file) as f:
         for i, sent in enumerate(f):
-            if not opt.keep_case:
+            if not keep_case:
                 sent = sent.lower()
             words = sent.split()
-            word_inst = words[:opt.max_seq_len]
+            word_inst = words[:max_seq_len]
             word_insts += [[Constants.BOS_WORD] + word_inst + [Constants.EOS_WORD]]
 
     print('[Info] Get {} instances from {}'.format(len(word_insts), inst_file))
     return word_insts
 
-def build_vocab_idx(word_insts):
+def build_vocab_idx(word_insts, min_word_count):
     ''' Trim vocab by number of occurence '''
 
     full_vocab = set(w for sent in word_insts for w in sent)
@@ -52,13 +39,13 @@ def build_vocab_idx(word_insts):
     ignored_word_count = 0
     for word, count in word_count.items():
         if word not in word2idx:
-            if count > opt.min_word_count:
+            if count > min_word_count:
                 word2idx[word] = len(word2idx)
             else:
                 ignored_word_count += 1
 
     print('[Info] Trimmed vocabulary size = {},'.format(len(word2idx)),
-          'each with minimum occurrence = {}'.format(opt.min_word_count))
+          'each with minimum occurrence = {}'.format(min_word_count))
     print("[Info] Ignored word count = {}".format(ignored_word_count))
     return word2idx
 
@@ -69,8 +56,21 @@ def convert_instance_to_idx_seq(word_insts, word2idx):
 def main():
     ''' Main function '''
 
-    train_src_word_insts = read_instances_from_file(opt.train_src)
-    train_tgt_word_insts = read_instances_from_file(opt.train_tgt)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-train_src', required=True)
+    parser.add_argument('-train_tgt', required=True)
+    parser.add_argument('-valid_src', required=True)
+    parser.add_argument('-valid_tgt', required=True)
+    parser.add_argument('-output', required=True)
+    parser.add_argument('-max_seq_len', type=int, default=20)
+    parser.add_argument('-min_word_count', type=int, default=5)
+    parser.add_argument('-keep_case', action='store_true')
+    parser.add_argument('-share_vocab', action='store_true')
+
+    opt = parser.parse_args()
+
+    train_src_word_insts = read_instances_from_file(opt.train_src, opt.max_seq_len, opt.keep_case)
+    train_tgt_word_insts = read_instances_from_file(opt.train_tgt, opt.max_seq_len, opt.keep_case)
 
     if len(train_src_word_insts) != len(train_tgt_word_insts):
         print('[Warning] The training instance count is not equal.')
@@ -78,8 +78,8 @@ def main():
         train_src_word_insts = train_src_word_insts[:min_inst_count]
         train_tgt_word_insts = train_tgt_word_insts[:min_inst_count]
 
-    valid_src_word_insts = read_instances_from_file(opt.valid_src)
-    valid_tgt_word_insts = read_instances_from_file(opt.valid_tgt)
+    valid_src_word_insts = read_instances_from_file(opt.valid_src, opt.max_seq_len, opt.keep_case)
+    valid_tgt_word_insts = read_instances_from_file(opt.valid_tgt, opt.max_seq_len, opt.keep_case)
 
     if len(valid_src_word_insts) != len(valid_tgt_word_insts):
         print('[Warning] The validation instance count is not equal.')
@@ -89,13 +89,14 @@ def main():
 
     if opt.share_vocab:
         print('[Info] Build shared vocabulary for source and target.')
-        word2idx = build_vocab_idx(train_src_word_insts + train_tgt_word_insts)
+        word2idx = build_vocab_idx(
+            train_src_word_insts + train_tgt_word_insts, opt.min_word_count)
         src_word2idx = tgt_word2idx = word2idx
     else:
         print('[Info] Build vocabulary for source.')
-        src_word2idx = build_vocab_idx(train_src_word_insts)
+        src_word2idx = build_vocab_idx(train_src_word_insts, opt.min_word_count)
         print('[Info] Build vocabulary for target.')
-        tgt_word2idx = build_vocab_idx(train_tgt_word_insts)
+        tgt_word2idx = build_vocab_idx(train_tgt_word_insts, opt.min_word_count)
 
     print('[Info] Convert source word instances into sequences of word index.')
     train_src_insts = convert_instance_to_idx_seq(train_src_word_insts, src_word2idx)
@@ -106,7 +107,7 @@ def main():
     valid_tgt_insts = convert_instance_to_idx_seq(valid_tgt_word_insts, tgt_word2idx)
 
     data = {
-        'setting': opt,
+        'settings': opt,
         'dict': {
             'src': src_word2idx,
             'tgt': tgt_word2idx},
