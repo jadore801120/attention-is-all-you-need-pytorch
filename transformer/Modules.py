002815545bf743d0bf2b9d172af047b37e20b6e9
Victor Huang <jadore801120@gmail.com>
2017-06-25 00:29:49 +0800

    add attn mask shape check

--- transformer/Modules.py
+++ transformer/Modules.py
@@ -29,6 +29,10 @@ class BottleLinear(Bottle, Linear):
     ''' Perform the reshape routine before and after a linear projection '''
     pass
 
+class BottleSoftmax(Bottle, nn.Softmax):
+    ''' Perform the reshape routine before and after a softmax operation'''
+    pass
+
 class LayerNormalization(nn.Module):
     ''' Layer normalization module '''
 
@@ -57,18 +61,23 @@ class ScaledDotProductAttention(nn.Module):
         super(ScaledDotProductAttention, self).__init__()
         self.temper = np.power(d_model, 0.5)
         self.dropout = nn.Dropout(attn_dropout)
-        self.softmax = nn.Softmax()
+        self.softmax = BottleSoftmax()
 
     def forward(self, q, k, v, attn_mask=None):
-        _, len_q, _ = q.size()
-        _, len_k, _ = k.size()
 
         attn = torch.bmm(q, k.transpose(1, 2)) / self.temper
 
         if attn_mask is not None:
+
+            assert attn_mask.size() == attn.size(), \
+                    'Attention mask shape {} mismatch ' \
+                    'with Attention logit tensor shape ' \
+                    '{}.'.format(attn_mask.size(), attn.size())
+
             attn.data.masked_fill_(attn_mask, -float('inf'))
 
-        attn = self.softmax(attn.view(-1, len_k)).view(-1, len_q, len_k)
+        attn = self.softmax(attn)
         attn = self.dropout(attn)
         output = torch.bmm(attn, v)
+
         return output, attn
