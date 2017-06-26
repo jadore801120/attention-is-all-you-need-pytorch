cc33f24845dd9773bbfaba93b7b1855eeb372990
Victor Huang <jadore801120@gmail.com>
2017-06-26 16:09:40 +0800

    batch bottle

--- transformer/Modules.py
+++ transformer/Modules.py
@@ -54,7 +54,17 @@ class LayerNormalization(nn.Module):
 
         return ln_out
 
-class BottleLayerNormalization(Bottle, LayerNormalization):
+class BatchBottle(nn.Module):
+    ''' Perform the reshape routine before and after an operation '''
+
+    def forward(self, input):
+        if len(input.size()) <= 2:
+            return super(BatchBottle, self).forward(input)
+        size = input.size()[1:]
+        out = super(BatchBottle, self).forward(input.view(-1, size[0]*size[1]))
+        return out.view(-1, size[0], size[1])
+
+class BottleLayerNormalization(BatchBottle, LayerNormalization):
     ''' Perform the reshape routine before and after a layer normalization'''
     pass
 
