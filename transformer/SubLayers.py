fix the layer normalization behavior

transformer/SubLayers.py
@@ -4,7 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.init as init
 from transformer.Modules import BottleLinear as Linear
-from transformer.Modules import ScaledDotProductAttention, LayerNormalization
+from transformer.Modules import ScaledDotProductAttention
+from transformer.Modules import BottleLayerNormalization as LayerNormalization
 
 __author__ = "Yu-Hsiang Huang"
 