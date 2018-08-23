300418eb98e1894f433dd0098b77c2f4d2f7c59f
yu-hsiang-huang <yhhuang@nlg.csie.ntu.edu.tw>
2018-08-23 18:11:02 +0800

    0.4

--- transformer/Modules.py
+++ transformer/Modules.py
@@ -13,13 +13,13 @@ class ScaledDotProductAttention(nn.Module):
         self.dropout = nn.Dropout(attn_dropout)
         self.softmax = nn.Softmax(dim=2)
 
-    def forward(self, q, k, v, attn_mask=None):
+    def forward(self, q, k, v, mask=None):
 
         attn = torch.bmm(q, k.transpose(1, 2))
         attn = attn / self.temperature
 
-        if attn_mask is not None:
-            attn = attn.masked_fill(attn_mask, -np.inf)
+        if mask is not None:
+            attn = attn.masked_fill(mask, -np.inf)
 
         attn = self.softmax(attn)
         attn = self.dropout(attn)
