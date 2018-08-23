29f8c5dfc5fdd9f3cc622fd7a3cadf42ef268dab
yu-hsiang-huang <yhhuang@nlg.csie.ntu.edu.tw>
2018-08-23 17:56:18 +0800

    0.4

--- transformer/Models.py
+++ transformer/Models.py
@@ -139,7 +139,8 @@ class Transformer(nn.Module):
             n_src_vocab, n_tgt_vocab, len_max_seq,
             d_word_vec=512, d_model=512, d_inner=1024,
             n_layers=6, n_head=8, d_k=64, d_v=64, dropout=0.1,
-            tgt_emb_prj_weight_sharing=True, emb_src_tgt_weight_sharing=True):
+            tgt_emb_prj_weight_sharing=True,
+            emb_src_tgt_weight_sharing=True):
 
         super().__init__()
 
