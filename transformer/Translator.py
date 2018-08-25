32bcc1026536114f887d703ae974d4ae5bfdb970
yu-hsiang-huang <yhhuang@nlg.csie.ntu.edu.tw>
2018-08-26 00:36:08 +0800

    0.4

--- transformer/Translator.py
+++ transformer/Translator.py
@@ -148,7 +148,7 @@ class Translator(object):
         def get_active_inst_idx_list(inst_beams, word_prob, inst_idx_to_position_map):
             active_inst_idx_list = []
             for inst_idx, inst_position in inst_idx_to_position_map.items():
-                is_inst_complete = inst_beams[inst_idx].advance(word_prob.data[inst_position])
+                is_inst_complete = inst_beams[inst_idx].advance(word_prob[inst_position])
                 if not is_inst_complete:
                     active_inst_idx_list += [inst_idx]
 
