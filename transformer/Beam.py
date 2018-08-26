b9c2389f7e712a7c0bc4ac4508e5df93bc482fb3
yu-hsiang-huang <yhhuang@nlg.csie.ntu.edu.tw>
2018-08-26 14:45:49 +0800

    .

--- transformer/Beam.py
+++ transformer/Beam.py
@@ -89,7 +89,7 @@ class Beam():
             _, keys = self.sort_scores()
             hyps = [self.get_hypothesis(k) for k in keys]
             hyps = [[Constants.BOS] + h for h in hyps]
-            dec_seq = torch.from_numpy(np.array(hyps))
+            dec_seq = torch.LongTensor(hyps)
 
         return dec_seq
 
