7e14834dd5e48bb1e6c74581c55684405e821298
Yu-Hsiang Huang <jadore801120@gmail.com>
2018-08-27 11:29:13 +0800

    minor

--- transformer/Models.py
+++ transformer/Models.py
@@ -14,13 +14,13 @@ def get_non_pad_mask(seq):
 def get_sinusoid_encoding_table(n_position, d_hid, padding_idx=None):
     ''' Sinusoid position encoding table '''
 
-    def cal_angle(hid_idx, position):
+    def cal_angle(position, hid_idx):
         return position / np.power(10000, 2 * (hid_idx // 2) / d_hid)
 
-    def get_posi_angle_vec(position, d_hid):
-        return [cal_angle(hid_j, position) for hid_j in range(d_hid)]
+    def get_posi_angle_vec(position):
+        return [cal_angle(position, hid_j) for hid_j in range(d_hid)]
 
-    sinusoid_table = np.array([get_posi_angle_vec(pos_i, d_hid) for pos_i in range(n_position)])
+    sinusoid_table = np.array([get_posi_angle_vec(pos_i) for pos_i in range(n_position)])
 
     sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
     sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
