750ffb075cab0dc1ceb3ed0a767dab9dfd692fed
Satyapriya Krishna <satyapk@amazon.com>
2018-09-26 11:18:34 -0700

    Added some cahnges

--- transformer/Models.py
+++ transformer/Models.py
@@ -85,7 +85,7 @@ class Encoder(nn.Module):
 
         # -- Forward
         enc_output = self.src_word_emb(src_seq) + self.position_enc(src_pos)
-
+        print("INPUT SEQ", src_seq)
         print("ENCODER BEGIN OUT : ", enc_output.shape)
         print("\n\n\n\n ->  -> -> Encoder ")
         for enc_layer in self.layer_stack:
