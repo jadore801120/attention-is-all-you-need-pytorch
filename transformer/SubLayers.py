1f4fae51bc55b9c078f19d9c7c9919fdebc16d60
yuanuayuan <784968994@qq.com>
2018-11-01 18:13:59 -0400

    Update SubLayers.py

--- transformer/SubLayers.py
+++ transformer/SubLayers.py
@@ -50,7 +50,8 @@ class MultiHeadAttention(nn.Module):
         k = k.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k) # (n*b) x lk x dk
         v = v.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v) # (n*b) x lv x dv
 
-        mask = mask.repeat(n_head, 1, 1) # (n*b) x .. x ..
+        if mask is not None:
+            mask = mask.repeat(n_head, 1, 1) # (n*b) x .. x ..
         output, attn = self.attention(q, k, v, mask=mask)
 
         output = output.view(n_head, sz_b, len_q, d_v)
