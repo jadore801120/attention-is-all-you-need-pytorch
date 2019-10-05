e304bbc2903f6b20119604c60c58c534d6534cbe
liht <liht@dsp.ac.cn>
2019-10-05 16:56:22 +0800

    new

---
+++ prepare.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+for l in en de; do for f in data/multi30k/*.$l; do if [[ "$f" != *"test"* ]]; then sed -i "$ d" $f; fi;  done; done
+for l in en de; do for f in data/multi30k/*.$l; do perl tokenizer.perl -a -no-escape -l $l -q  < $f > $f.atok; done; done
+python preprocess.py -train_src data/multi30k/train.en.atok -train_tgt data/multi30k/train.de.atok -valid_src data/multi30k/val.en.atok -valid_tgt data/multi30k/val.de.atok -save_data data/multi30k.atok.low.pt
