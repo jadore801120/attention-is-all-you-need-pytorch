1a8c388aa650c80d896b7a7bf479b17861d09c30
Yu-Hsiang Huang <jadore801120@gmail.com>
2019-11-27 17:10:52 +0800

    fix positionwise feed forward

--- transformer/SubLayers.py
+++ transformer/SubLayers.py
@@ -61,21 +61,20 @@ class MultiHeadAttention(nn.Module):
 
         return output, attn
 
+
 class PositionwiseFeedForward(nn.Module):
     ''' A two-feed-forward-layer module '''
 
     def __init__(self, d_in, d_hid, dropout=0.1):
         super().__init__()
-        self.w_1 = nn.Conv1d(d_in, d_hid, 1) # position-wise
-        self.w_2 = nn.Conv1d(d_hid, d_in, 1) # position-wise
+        self.w_1 = nn.Linear(d_in, d_hid) # position-wise
+        self.w_2 = nn.Linear(d_hid, d_in) # position-wise
         self.layer_norm = nn.LayerNorm(d_in)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         residual = x
-        output = x.transpose(1, 2)
-        output = self.w_2(F.relu(self.w_1(output)))
-        output = output.transpose(1, 2)
+        output = self.w_2(F.relu(self.w_1(x)))
         output = self.dropout(output)
         output = self.layer_norm(output + residual)
         return output
