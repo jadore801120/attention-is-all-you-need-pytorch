676ae2e7d82f3653b5a07a03c2d255dd5740e017
Yu-Hsiang Huang <jadore801120@gmail.com>
2019-11-28 16:10:25 +0800

    refine preprocessing with the powerful spacy

--- preprocess.py
+++ preprocess.py
@@ -1,164 +1,85 @@
 ''' Handling the data io '''
 import argparse
+import spacy
 import torch
+import dill as pickle
+import torchtext.data
+import torchtext.datasets
 import transformer.Constants as Constants
 
-def read_instances_from_file(inst_file, max_sent_len, keep_case):
-    ''' Convert file into word seq lists and vocab '''
-
-    word_insts = []
-    trimmed_sent_count = 0
-    with open(inst_file) as f:
-        for sent in f:
-            if not keep_case:
-                sent = sent.lower()
-            words = sent.split()
-            if len(words) > max_sent_len:
-                trimmed_sent_count += 1
-            word_inst = words[:max_sent_len]
-
-            if word_inst:
-                word_insts += [[Constants.BOS_WORD] + word_inst + [Constants.EOS_WORD]]
-            else:
-                word_insts += [None]
-
-    print('[Info] Get {} instances from {}'.format(len(word_insts), inst_file))
-
-    if trimmed_sent_count > 0:
-        print('[Warning] {} instances are trimmed to the max sentence length {}.'
-              .format(trimmed_sent_count, max_sent_len))
-
-    return word_insts
-
-def build_vocab_idx(word_insts, min_word_count):
-    ''' Trim vocab by number of occurence '''
-
-    full_vocab = set(w for sent in word_insts for w in sent)
-    print('[Info] Original Vocabulary size =', len(full_vocab))
-
-    word2idx = {
-        Constants.BOS_WORD: Constants.BOS,
-        Constants.EOS_WORD: Constants.EOS,
-        Constants.PAD_WORD: Constants.PAD,
-        Constants.UNK_WORD: Constants.UNK}
-
-    word_count = {w: 0 for w in full_vocab}
-
-    for sent in word_insts:
-        for word in sent:
-            word_count[word] += 1
-
-    ignored_word_count = 0
-    for word, count in word_count.items():
-        if word not in word2idx:
-            if count > min_word_count:
-                word2idx[word] = len(word2idx)
-            else:
-                ignored_word_count += 1
-
-    print('[Info] Trimmed vocabulary size = {},'.format(len(word2idx)),
-          'each with minimum occurrence = {}'.format(min_word_count))
-    print("[Info] Ignored word count = {}".format(ignored_word_count))
-    return word2idx
-
-def convert_instance_to_idx_seq(word_insts, word2idx):
-    ''' Mapping words to idx sequence. '''
-    return [[word2idx.get(w, Constants.UNK) for w in s] for s in word_insts]
-
 def main():
-    ''' Main function '''
+    '''
+    Usage: python preprocess.py -lang_src de -lang_trg en -save_data multi30k_de_en.pt
+    '''
+
+    spacy_support_langs = ['de', 'el', 'en', 'es', 'fr', 'it', 'lt', 'nb', 'nl', 'pt']
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-train_src', required=True)
-    parser.add_argument('-train_tgt', required=True)
-    parser.add_argument('-valid_src', required=True)
-    parser.add_argument('-valid_tgt', required=True)
+    parser.add_argument('-lang_src', required=True, choices=spacy_support_langs)
+    parser.add_argument('-lang_trg', required=True, choices=spacy_support_langs)
     parser.add_argument('-save_data', required=True)
-    parser.add_argument('-max_len', '--max_word_seq_len', type=int, default=50)
-    parser.add_argument('-min_word_count', type=int, default=5)
+    parser.add_argument('-data_src', type=str, default=None)
+    parser.add_argument('-data_trg', type=str, default=None)
+
+    parser.add_argument('-max_len', type=int, default=100)
+    parser.add_argument('-min_word_count', type=int, default=3)
     parser.add_argument('-keep_case', action='store_true')
-    parser.add_argument('-share_vocab', action='store_true')
-    parser.add_argument('-vocab', default=None)
+    #parser.add_argument('-ratio', '--train_valid_test_ratio', type=int, nargs=3, metavar=(8,1,1))
+    #parser.add_argument('-share_vocab', action='store_true')
+    #parser.add_argument('-vocab', default=None)
 
     opt = parser.parse_args()
-    opt.max_token_seq_len = opt.max_word_seq_len + 2 # include the <s> and </s>
+    assert not any([opt.data_src, opt.data_trg]), 'Custom data input is not support now.'
+    assert not any([opt.data_src, opt.data_trg]) or all([opt.data_src, opt.data_trg])
+    print(opt)
 
-    # Training set
-    train_src_word_insts = read_instances_from_file(
-        opt.train_src, opt.max_word_seq_len, opt.keep_case)
-    train_tgt_word_insts = read_instances_from_file(
-        opt.train_tgt, opt.max_word_seq_len, opt.keep_case)
+    src_lang_model = spacy.load(opt.lang_src)
+    trg_lang_model = spacy.load(opt.lang_trg)
 
-    if len(train_src_word_insts) != len(train_tgt_word_insts):
-        print('[Warning] The training instance count is not equal.')
-        min_inst_count = min(len(train_src_word_insts), len(train_tgt_word_insts))
-        train_src_word_insts = train_src_word_insts[:min_inst_count]
-        train_tgt_word_insts = train_tgt_word_insts[:min_inst_count]
+    def tokenize_src(text):
+        return [tok.text for tok in src_lang_model.tokenizer(text)]
 
-    #- Remove empty instances
-    train_src_word_insts, train_tgt_word_insts = list(zip(*[
-        (s, t) for s, t in zip(train_src_word_insts, train_tgt_word_insts) if s and t]))
+    def tokenize_trg(text):
+        return [tok.text for tok in trg_lang_model.tokenizer(text)]
 
-    # Validation set
-    valid_src_word_insts = read_instances_from_file(
-        opt.valid_src, opt.max_word_seq_len, opt.keep_case)
-    valid_tgt_word_insts = read_instances_from_file(
-        opt.valid_tgt, opt.max_word_seq_len, opt.keep_case)
+    SRC = torchtext.data.Field(
+        tokenize=tokenize_src, lower=not opt.keep_case,
+        pad_token=Constants.PAD_WORD, init_token=Constants.BOS_WORD, eos_token=Constants.EOS_WORD)
 
-    if len(valid_src_word_insts) != len(valid_tgt_word_insts):
-        print('[Warning] The validation instance count is not equal.')
-        min_inst_count = min(len(valid_src_word_insts), len(valid_tgt_word_insts))
-        valid_src_word_insts = valid_src_word_insts[:min_inst_count]
-        valid_tgt_word_insts = valid_tgt_word_insts[:min_inst_count]
+    TRG = torchtext.data.Field(
+        tokenize=tokenize_trg, lower=not opt.keep_case,
+        pad_token=Constants.PAD_WORD, init_token=Constants.BOS_WORD, eos_token=Constants.EOS_WORD)
 
-    #- Remove empty instances
-    valid_src_word_insts, valid_tgt_word_insts = list(zip(*[
-        (s, t) for s, t in zip(valid_src_word_insts, valid_tgt_word_insts) if s and t]))
+    MAX_LEN = opt.max_len
+    MIN_FREQ = opt.min_word_count
 
-    # Build vocabulary
-    if opt.vocab:
-        predefined_data = torch.load(opt.vocab)
-        assert 'dict' in predefined_data
-
-        print('[Info] Pre-defined vocabulary found.')
-        src_word2idx = predefined_data['dict']['src']
-        tgt_word2idx = predefined_data['dict']['tgt']
+    if not all([opt.data_src, opt.data_trg]):
+        assert {opt.lang_src, opt.lang_trg} == {'de', 'en'}
     else:
-        if opt.share_vocab:
-            print('[Info] Build shared vocabulary for source and target.')
-            word2idx = build_vocab_idx(
-                train_src_word_insts + train_tgt_word_insts, opt.min_word_count)
-            src_word2idx = tgt_word2idx = word2idx
-        else:
-            print('[Info] Build vocabulary for source.')
-            src_word2idx = build_vocab_idx(train_src_word_insts, opt.min_word_count)
-            print('[Info] Build vocabulary for target.')
-            tgt_word2idx = build_vocab_idx(train_tgt_word_insts, opt.min_word_count)
+        # Pack custom txt file into example datasets
+        raise NotImplementedError
 
-    # word to index
-    print('[Info] Convert source word instances into sequences of word index.')
-    train_src_insts = convert_instance_to_idx_seq(train_src_word_insts, src_word2idx)
-    valid_src_insts = convert_instance_to_idx_seq(valid_src_word_insts, src_word2idx)
+    def filter_examples_with_length(x):
+        return len(vars(x)['src']) <= MAX_LEN and len(vars(x)['trg']) <= MAX_LEN
 
-    print('[Info] Convert target word instances into sequences of word index.')
-    train_tgt_insts = convert_instance_to_idx_seq(train_tgt_word_insts, tgt_word2idx)
-    valid_tgt_insts = convert_instance_to_idx_seq(valid_tgt_word_insts, tgt_word2idx)
+    train, val, test = torchtext.datasets.Multi30k.splits(
+            exts = ('.' + opt.lang_src, '.' + opt.lang_trg),
+            #split_ratio=opt.train_valid_test_ratio,
+            fields = (SRC, TRG),
+            filter_pred=filter_examples_with_length)
+
+    SRC.build_vocab(train.src, min_freq=MIN_FREQ)
+    TRG.build_vocab(train.trg, min_freq=MIN_FREQ)
 
     data = {
         'settings': opt,
-        'dict': {
-            'src': src_word2idx,
-            'tgt': tgt_word2idx},
-        'train': {
-            'src': train_src_insts,
-            'tgt': train_tgt_insts},
-        'valid': {
-            'src': valid_src_insts,
-            'tgt': valid_tgt_insts}}
+        'vocab': {'src': SRC, 'trg': TRG},
+        'train': train.examples,
+        'valid': val.examples,
+        'test': test.examples}
 
     print('[Info] Dumping the processed data to pickle file', opt.save_data)
-    torch.save(data, opt.save_data)
-    print('[Info] Finish.')
+    pickle.dump(data, open(opt.save_data, 'wb'))
 
 if __name__ == '__main__':
     main()
