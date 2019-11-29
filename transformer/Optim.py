f9f4587661731a334408d94a6bb79593a088c444
Yu-Hsiang Huang <jadore801120@gmail.com>
2019-11-29 11:55:47 +0800

    just coding style

--- transformer/Optim.py
+++ transformer/Optim.py
@@ -1,4 +1,4 @@
-'''A wrapper class for optimizer '''
+'''A wrapper class for scheduled optimizer '''
 import numpy as np
 
 class ScheduledOptim():
@@ -6,23 +6,26 @@ class ScheduledOptim():
 
     def __init__(self, optimizer, d_model, n_warmup_steps):
         self._optimizer = optimizer
-        self.n_warmup_steps = n_warmup_steps
-        self.n_current_steps = 0
         self.init_lr = np.power(d_model, -0.5)
+        self.n_warmup_steps = n_warmup_steps
+        self.n_steps = 0
+
 
     def step_and_update_lr(self):
         "Step with the inner optimizer"
         self._update_learning_rate()
         self._optimizer.step()
 
+
     def zero_grad(self):
-        "Zero out the gradients by the inner optimizer"
+        "Zero out the gradients with the inner optimizer"
         self._optimizer.zero_grad()
 
+
     def _get_lr_scale(self):
-        return np.min([
-            np.power(self.n_current_steps, -0.5),
-            np.power(self.n_warmup_steps, -1.5) * self.n_current_steps])
+        n_steps, n_warmup_steps = self.n_steps, self.n_warmup_steps
+        return min(n_steps ** (-0.5), n_steps * n_warmup_steps ** (-1.5))
+
 
     def _update_learning_rate(self):
         ''' Learning rate scheduling per step '''
