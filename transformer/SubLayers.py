58d0abaa93f7f35febfc8e06cd79e9ed7c57b3e4
Yu-Hsiang Huang <jadore801120@gmail.com>
2019-11-30 17:18:06 +0800

    minor

--- transformer/SubLayers.py
+++ transformer/SubLayers.py
@@ -6,8 +6,6 @@ from transformer.Modules import ScaledDotProductAttention
 
 __author__ = "Yu-Hsiang Huang"
 
-
-
 class MultiHeadAttention(nn.Module):
     ''' Multi-Head Attention module '''
 
@@ -23,10 +21,10 @@ class MultiHeadAttention(nn.Module):
         self.w_vs = nn.Linear(d_model, n_head * d_v, bias=False)
         self.fc = nn.Linear(n_head * d_v, d_model, bias=False)
 
-        self.attention = ScaledDotProductAttention(temperature=np.power(d_k, 0.5))
+        self.attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
 
         self.dropout = nn.Dropout(dropout)
-        self.layer_norm = nn.LayerNorm(d_model)
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
 
 
     def forward(self, q, k, v, mask=None):
@@ -47,7 +45,7 @@ class MultiHeadAttention(nn.Module):
         q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
         
         if mask is not None:
-            mask = mask.unsqueeze(1)
+            mask = mask.unsqueeze(1)   # For head axis broadcasting.
 
         output, attn = self.attention(q, k, v, mask=mask)
 
@@ -67,7 +65,7 @@ class PositionwiseFeedForward(nn.Module):
         super().__init__()
         self.w_1 = nn.Linear(d_in, d_hid) # position-wise
         self.w_2 = nn.Linear(d_hid, d_in) # position-wise
-        self.layer_norm = nn.LayerNorm(d_in)
+        self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
