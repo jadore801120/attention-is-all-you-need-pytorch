2c21b130af3276b908f8ad85ec48d418b2ef60af
Yu-Hsiang Huang <jadore801120@gmail.com>
2019-12-08 16:02:04 +0800

    .

--- transformer/__init__.py
+++ transformer/__init__.py
@@ -4,10 +4,9 @@ import transformer.Layers
 import transformer.SubLayers
 import transformer.Models
 import transformer.Translator
-import transformer.Beam
 import transformer.Optim
 
 __all__ = [
     transformer.Constants, transformer.Modules, transformer.Layers,
     transformer.SubLayers, transformer.Models, transformer.Optim,
-    transformer.Translator, transformer.Beam]
+    transformer.Translator]
