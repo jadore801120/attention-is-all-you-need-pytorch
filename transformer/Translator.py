3ab11cc8956c30878c8256f1b5796767376de53b
Yu-Hsiang Huang <jadore801120@gmail.com>
2019-12-22 21:48:19 +0800

    fix the shape bug of the scores, thanks to @Zessay

--- transformer/Translator.py
+++ transformer/Translator.py
@@ -48,9 +48,9 @@ class Translator(nn.Module):
         enc_output, *_ = self.model.encoder(src_seq, src_mask)
         dec_output = self._model_decode(self.init_seq, enc_output, src_mask)
         
-        best_k_probs, best_k_idx = dec_output[:, -1].topk(beam_size)
+        best_k_probs, best_k_idx = dec_output[:, -1, :].topk(beam_size)
 
-        scores = torch.log(best_k_probs.unsqueeze(0))
+        scores = torch.log(best_k_probs).view(beam_size)
         gen_seq = self.blank_seqs.clone().detach()
         gen_seq[:, 1] = best_k_idx[0]
         enc_output = enc_output.repeat(beam_size, 1, 1)
@@ -58,23 +58,28 @@ class Translator(nn.Module):
 
 
     def _get_the_best_score_and_idx(self, gen_seq, dec_output, scores, step):
+        assert len(scores.size()) == 1
         
         beam_size = self.beam_size
 
         # Get k candidates for each beam, k^2 candidates in total.
         best_k2_probs, best_k2_idx = dec_output[:, -1, :].topk(beam_size)
-        best_k2_probs = torch.log(best_k2_probs).view(beam_size, -1) + scores.transpose(0, 1)
+
+        # Include the previous scores.
+        scores = torch.log(best_k2_probs).view(beam_size, -1) + scores.view(beam_size, 1)
 
         # Get the best k candidates from k^2 candidates.
-        best_k_probs, best_k_idx_in_k2 = best_k2_probs.view(-1).topk(beam_size)
+        scores, best_k_idx_in_k2 = scores.view(-1).topk(beam_size)
+ 
+        # Get the corresponding positions of the best k candidiates.
         best_k_r_idxs, best_k_c_idxs = best_k_idx_in_k2 // beam_size, best_k_idx_in_k2 % beam_size
         best_k_idx = best_k2_idx[best_k_r_idxs, best_k_c_idxs]
 
+        # Copy the corresponding previous tokens.
         gen_seq[:, :step] = gen_seq[best_k_r_idxs, :step]
+        # Set the best tokens in this beam search step
         gen_seq[:, step] = best_k_idx
 
-        scores = best_k_probs.unsqueeze(0)
-        
         return gen_seq, scores
 
 
@@ -103,7 +108,7 @@ class Translator(nn.Module):
                 # -- check if all beams contain eos
                 if (eos_locs.sum(1) > 0).sum(0).item() == beam_size:
                     # TODO: Try different terminate conditions.
-                    _, ans_idx = scores.div(seq_lens.float() ** alpha).max(1)
+                    _, ans_idx = scores.div(seq_lens.float() ** alpha).max(0)
                     ans_idx = ans_idx.item()
                     break
         return gen_seq[ans_idx][:seq_lens[ans_idx]].tolist()
